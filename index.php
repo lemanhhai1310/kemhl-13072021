7334ddc97f054c9b7480b3d38ed8f092c766b45f
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-07-23 21:10:53 +0700

    update

--- index.php
+++ index.php
@@ -41,7 +41,44 @@
 <!--Khach Hang-->
 <div class="home__kh uk-section-small uk-background-norepeat uk-background-top-center" data-src="images/Group23873.png" uk-img>
     <div class="uk-container uk-container-small">
+        <div uk-slider>
 
+            <div class="uk-position-relative">
+
+                <div class="uk-slider-container">
+                    <ul class="uk-slider-items uk-child-width-1-1">
+                        <?php for ($i=0;$i<3;$i++): ?>
+                        <li>
+                            <div class="uk-text-center">
+                                <div class="uk-cover-container uk-border-circle uk-display-inline-block">
+                                    <img src="images/avarta.png" alt="" uk-cover>
+                                    <canvas width="292" height="292"></canvas>
+                                </div>
+                            </div>
+                            <div class="uk-text-center">
+                                <h3 class="uk-h3">Phương Linh</h3>
+                                <div>Nhà mình từ ông bà, bố mẹ cho đến con cháu đều thích ăn kem Hùng Linh hết ấy. Trong tủ lạnh lúc nào cũng có mấy hộp kem Hùng Linh, chật kín cả tủ luôn.</div>
+                            </div>
+                        </li>
+                        <?php endfor; ?>
+                    </ul>
+                </div>
+
+                <div class="uk-hidden@s uk-light">
+                    <a class="uk-position-center-left uk-position-small" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                    <a class="uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slider-item="next"></a>
+                </div>
+
+                <div class="uk-visible@s">
+                    <a class="uk-position-center-left-out uk-position-small block_tintuc__arrow block_tintuc__arrow--next" href="#" uk-slider-item="previous"><img src="images/icons8-left.png" alt=""></a>
+                    <a class="uk-position-center-right-out uk-position-small block_tintuc__arrow block_tintuc__arrow--prev" href="#" uk-slider-item="next"><img src="images/icons8-right.png" alt=""></a>
+                </div>
+
+            </div>
+
+            <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+
+        </div>
     </div>
 </div>
 <!--End Khach Hang -->
